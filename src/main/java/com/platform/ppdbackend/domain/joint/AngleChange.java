fc124d5e94013ad06b5389c82a1cf99e909edc88
juya <kjy2967@naver.com>
2023-05-28 18:45:08 +0900

    EDIT : #1 특성별 결과 타입 추가, graph url , detail 삭제

--- src/main/java/com/platform/ppdbackend/domain/joint/AngleChange.java
+++ src/main/java/com/platform/ppdbackend/domain/joint/AngleChange.java
@@ -3,6 +3,7 @@ package com.platform.ppdbackend.domain.joint;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.platform.ppdbackend.domain.BaseTimeEntity;
+import com.platform.ppdbackend.domain.joint.enums.ResultType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,12 +19,12 @@ public class AngleChange extends BaseTimeEntity {
     @Column(name="angle_idx")
     private Long idx;
 
-    private String graphUrl;
-    private String detail; // 추가 설명
-
     private Float actual_value;
     private Float normal_value;
     private Float abnormal_value;
+
+    @Enumerated(EnumType.STRING)
+    private ResultType resultType; // 각 특성의 결과상태(정상/비정상)을 관리할 Enum class
 
     // 읽기 전용
     @OneToOne(mappedBy = "angleChange") // Joint 엔티티에 있는 angleChange 필드와 매핑되었다.
--- src/main/java/com/platform/ppdbackend/domain/joint/Joint.java
+++ src/main/java/com/platform/ppdbackend/domain/joint/Joint.java
@@ -27,7 +27,7 @@ public class Joint extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private ResultType resultType; // 각 사용자의 결과상태(정상/비정상)을 관리할 Enum class
 
-    // 4가지 특성 1:1매칭
+    // 6가지 특성 1:1매칭
     @OneToOne
     @JoinColumn(name="speed_idx")
     private Speed speed;
@@ -43,6 +43,14 @@ public class Joint extends BaseTimeEntity {
     @OneToOne
     @JoinColumn(name="angle_idx")
     private AngleChange angleChange;
+
+    @OneToOne
+    @JoinColumn(name="back_idx")
+    private BackAngle backAngle;
+
+    @OneToOne
+    @JoinColumn(name="arm_idx")
+    private ArmAngle armAngle;
 
     // Joint : User = N: 1 관계 (사용자는 많은 결과를 가질 수 있다)
     // 연관관계의 주인
