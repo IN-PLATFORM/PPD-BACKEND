9d71b6523f274396476e2ef353087c86818aa17f
juchan <schultz19@naver.com>
2023-05-30 00:02:39 +0900

    feat : Webconfig 추가

--- src/main/java/com/platform/ppdbackend/domain/config/SecurityConfig.java
+++ src/main/java/com/platform/ppdbackend/domain/config/SecurityConfig.java
@@ -1,2 +1,19 @@
-package com.platform.ppdbackend.domain.config;public class SecurityConfig {
+package com.platform.ppdbackend.domain.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends SecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/**").permitAll();
+    }
 }
