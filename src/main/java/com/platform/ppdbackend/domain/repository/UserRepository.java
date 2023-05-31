b8cfad80a30b99bc4b90f946052d337ec2de36d2
juchan <schultz19@naver.com>
2023-05-31 14:21:13 +0900

    feat : #1 리포지터리 설계

--- src/main/java/com/platform/ppdbackend/domain/repository/UserRepository.java
+++ src/main/java/com/platform/ppdbackend/domain/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package com.platform.ppdbackend.domain.repository;
 
 
-import com.platform.ppdbackend.domain.user.User;
+import com.platform.ppdbackend.domain.user.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmail(String email);
 
     boolean existsByEmail(String email);
 }
