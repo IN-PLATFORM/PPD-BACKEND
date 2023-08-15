a8d303ddd422783205a6c4950e1037d66e73b934
juya <kjy2967@naver.com>
2023-08-16 05:10:10 +0900

    [FEAT] : #12 생성자 생성

--- src/main/java/com/platform/ppdbackend/domain/joint/BackAngle.java
+++ src/main/java/com/platform/ppdbackend/domain/joint/BackAngle.java
@@ -29,4 +29,11 @@ public class BackAngle extends BaseTimeEntity {
     // 읽기 전용
     @OneToOne(mappedBy = "backAngle") // Joint 엔티티에 있는 angleChange 필드와 매핑되었다.
     private Joint joint;
+
+    public BackAngle(Float actual_value, Float normal_value, Float abnormal_value, ResultType resultType) {
+        this.actual_value = actual_value;
+        this.normal_value = normal_value;
+        this.abnormal_value = abnormal_value;
+        this.resultType = resultType;
+    }
 }
